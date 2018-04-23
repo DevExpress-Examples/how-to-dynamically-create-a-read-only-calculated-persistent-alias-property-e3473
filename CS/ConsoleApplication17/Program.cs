04b73ee6efcd2038fa39f39f289a50b193e39c22
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ConsoleApplication17/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+
+namespace ConsoleApplication17 {
+    class Program {
+        static void Main(string[] args) {
+
+            XpoDefault.DataLayer = new SimpleDataLayer(new DevExpress.Xpo.DB.InMemoryDataStore());
+            XpoDefault.Session = null;
+
+            using (UnitOfWork uow = new UnitOfWork()) {
+                uow.ClearDatabase();
+                TestClass c1 = new TestClass(uow);
+                c1.Name = "aaa"; c1.Email = "e@mail.com";
+                TestClass c2 = new TestClass(uow);
+                c2.Name = "bbb"; c2.Email = "nobody@w3.com";
+                c2.Master = c1;
+                uow.CommitChanges();
+            }
+
+            XPClassInfo ci = XpoDefault.DataLayer.Dictionary.GetClassInfo(typeof(TestClass));
+            ci.CreateAliasedMember("DisplayName", typeof(string), "concat([Name],' (',[Email],')',iif([Master] is null,'',Concat(' managed by ',[Master].Name)))");
+
+            using (UnitOfWork uow = new UnitOfWork()) {
+                XPCollection<TestClass> xpc = new XPCollection<TestClass>(uow, CriteriaOperator.Parse("Contains([DisplayName],'w3')"));
+                System.Diagnostics.Debug.Assert(xpc.Count == 1);
+                System.Diagnostics.Debug.Assert(xpc[0].Name == "bbb");
+                XPMemberInfo mi = xpc[0].ClassInfo.FindMember("DisplayName");
+                System.Diagnostics.Debug.Assert(mi!= null && mi.IsReadOnly && mi.IsAliased);
+                System.Diagnostics.Debug.Assert(object.Equals(mi.GetValue(xpc[0]), "bbb (nobody@w3.com) managed by aaa"));
+            }
+        }
+    }
+
+    public class TestClass : XPObject {
+        public TestClass(Session s) : base(s) { }
+
+        public string Name {
+            get { return GetPropertyValue<string>("Name"); }
+            set { SetPropertyValue<string>("Name", value); }
+        }
+        public string Email {
+            get { return GetPropertyValue<string>("Email"); }
+            set { SetPropertyValue<string>("Email", value); }
+        }
+        public TestClass Master {
+            get { return GetPropertyValue<TestClass>("Master"); }
+            set { SetPropertyValue<TestClass>("Master", value); }
+        }
+    }
+}
---
+++ VB/ConsoleApplication17/XPAliasedMemberInfo.vb
@@ -0,0 +1,81 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Data.Filtering.Helpers
+
+Namespace DevExpress.Xpo.Metadata
+	Public Class XPAliasedMemberInfo
+		Inherits XPMemberInfo
+		Private ReadOnly propertyName As String
+		Private ReadOnly propertyType As Type
+		Public Sub New(ByVal owner As XPClassInfo, ByVal propertyName As String, ByVal propertyType As Type, ByVal expression As String)
+			MyBase.New(owner, True)
+			If propertyName Is Nothing Then
+				Throw New ArgumentNullException("propertyName")
+			End If
+			Me.propertyName = propertyName
+			Me.propertyType = propertyType
+			Owner.AddMember(Me)
+			Me.AddAttribute(New PersistentAliasAttribute(expression))
+		End Sub
+		Public Overrides ReadOnly Property Name() As String
+			Get
+				Return propertyName
+			End Get
+		End Property
+		Public Overrides ReadOnly Property IsPublic() As Boolean
+			Get
+				Return True
+			End Get
+		End Property
+		Public Overrides ReadOnly Property MemberType() As Type
+			Get
+				Return propertyType
+			End Get
+		End Property
+		Protected Overrides ReadOnly Property CanPersist() As Boolean
+			Get
+				Return False
+			End Get
+		End Property
+		Public Overrides Function GetValue(ByVal theObject As Object) As Object
+			Dim caseSensitive As Boolean = XpoDefault.DefaultCaseSensitive
+			If TypeOf theObject Is DevExpress.Xpo.Helpers.ISessionProvider Then
+				caseSensitive = (CType(theObject, DevExpress.Xpo.Helpers.ISessionProvider)).Session.CaseSensitive
+			End If
+			Dim persistentAliasAttribute As PersistentAliasAttribute = CType(Me.GetAttributeInfo(GetType(PersistentAliasAttribute)), PersistentAliasAttribute)
+			Return New ExpressionEvaluator(Owner.GetEvaluatorContextDescriptor(), CriteriaOperator.Parse(persistentAliasAttribute.AliasExpression, New Object(){}), caseSensitive, Owner.Dictionary.CustomFunctionOperators).Evaluate(theObject)
+		End Function
+		Public Overrides Sub SetValue(ByVal theObject As Object, ByVal theValue As Object)
+		End Sub
+		Public Overrides Function GetModified(ByVal theObject As Object) As Boolean
+			Return False
+		End Function
+		Public Overrides Function GetOldValue(ByVal theObject As Object) As Object
+			Return GetValue(theObject)
+		End Function
+		Public Overrides Sub ResetModified(ByVal theObject As Object)
+		End Sub
+		Public Overrides Sub SetModified(ByVal theObject As Object, ByVal oldValue As Object)
+		End Sub
+	End Class
+
+	#Region ".NET Framework 3+ Extender"
+	Public Module XPAliasedMemberInfoExtender
+		<System.Runtime.CompilerServices.Extension> _
+		Public Function CreateAliasedMember(ByVal self As XPClassInfo, ByVal name As String, ByVal type As Type, ByVal expression As String) As XPAliasedMemberInfo
+			Return XPAliasedMemberInfoExtender.CreateAliasedMember(self, name, type, expression, Nothing)
+		End Function
+		<System.Runtime.CompilerServices.Extension> _
+		Public Function CreateAliasedMember(ByVal self As XPClassInfo, ByVal name As String, ByVal type As Type, ByVal expression As String, ByVal attrs() As Attribute) As XPAliasedMemberInfo
+			Dim result As New XPAliasedMemberInfo(self, name, type, expression)
+			If attrs IsNot Nothing Then
+				For Each a As Attribute In attrs
+					result.AddAttribute(a)
+				Next a
+			End If
+			Return result
+		End Function
+	End Module
+	#End Region
+End Namespace
